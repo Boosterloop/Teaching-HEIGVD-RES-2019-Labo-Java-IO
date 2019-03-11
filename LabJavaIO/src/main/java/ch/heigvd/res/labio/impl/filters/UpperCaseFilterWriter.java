c9baf44c153c07cfe78793c5625366d5717c17e5
Boosterloop <alison.savary@me.com>
2019-03-11 13:57:22 +0100

    Implementation of UpperCaseFilterWriter.java

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -16,17 +16,23 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // Convert all the string in upper case then call write of superclass
+    super.write(str.toUpperCase(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // For all characters in cbuf, convert to upper case
+    for(int i = 0; i < cbuf.length; i++) {
+      cbuf[i] = Character.toUpperCase(cbuf[i]);
+    }
+    // Call write of superclass
+    super.write(cbuf, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // Call write of superclass with the upper case version of char
+    super.write(Character.toUpperCase((char)c));
   }
-
 }
