579d29f718153fae4735f983ef43db8af36d98f9
Boosterloop <alison.savary@me.com>
2019-03-11 11:53:32 +0100

    First try to fix Application.java

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -90,6 +92,8 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote, "quotes-" + quote.getValue().getId() + ".utf8");
+
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +127,26 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    try{
+      String filePath = WORKSPACE_DIRECTORY;
+      for (String tag : quote.getTags()) {
+        filePath += "/" + tag;
+      }
+
+      // Create file and path
+      File file = new File(filePath);
+      file.getParentFile().mkdirs();
+
+      // Write quote in file
+      OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(filePath), "UTF8");
+      out.append(quote.getQuote());
+      out.flush();
+      out.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+
   }
   
   /**
@@ -140,6 +163,12 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try {
+          writer.write(file.getPath());
+          writer.flush();
+        } catch (IOException e){
+          System.out.println(e.getMessage());
+        }
       }
     });
   }
